094fb4369d113acb38336d0cf1f0c915e01ecb04
roedoejet <aidanpine@shaw.ca>
2019-11-11 14:52:46 -0800

    updated push

--- push.sh
+++ push.sh
@@ -5,17 +5,24 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
+move_exported_files() {
+    git checkout -b gh-pages
+    rm ${TRAVIS_BUILD_DIR}/assets/*.js
+    mv ${TRAVIS_BUILD_DIR}/*.js ${TRAVIS_BUILD_DIR}/assets/
+}
+
 commit_exported_files() {
-  git checkout -b build
-  git add ${TRAVIS_BUILD_DIR}/*.js
+  git checkout -b gh-pages
+  git add ${TRAVIS_BUILD_DIR}/assets/*.js
   git commit -a -m "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add travis-origin https://${GH_TOKEN}@github.com/roedoejet/mtd-starter.git > /dev/null 2>&1
-  git push travis-origin build
+  git push travis-origin gh-pages
 }
 
 setup_git
+move_exported_files
 commit_exported_files
 upload_files
