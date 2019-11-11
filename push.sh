845bd73cadcebee5ce833921f7678c9d5e468a25
roedoejet <aidanpine@shaw.ca>
2019-11-11 14:28:25 -0800

    checkout master

--- push.sh
+++ push.sh
@@ -6,13 +6,14 @@ setup_git() {
 }
 
 commit_exported_files() {
+  git checkout -b master
   git add ${TRAVIS_BUILD_DIR}/*.js
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add travis-origin https://${GH_TOKEN}@github.com/roedoejet/mtd-starter.git > /dev/null 2>&1
-  git push travis-origin master
+  git push --quiet --set-upstream travis-origin master
 }
 
 setup_git
